d0e1b7af7ddc0fc3049992116dd3e7fc83ba074b
syedumarali <57311114+syedumarali@users.noreply.github.com>
2024-02-26 15:01:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCs2wreOx5XlX2Rf-T7z3Qd5qXAxSC0rCM",
-    authDomain: "contact-form-7f6ca.firebaseapp.com",
-    databaseURL: "https://contact-form-7f6ca-default-rtdb.firebaseio.com",
-    projectId: "contact-form-7f6ca",
-    storageBucket: "contact-form-7f6ca.appspot.com",
-    messagingSenderId: "662192149007",
-    appId: "1:662192149007:web:accbb88121bc16bab5c97a"
+    
   };
 
   // initialize firebase
